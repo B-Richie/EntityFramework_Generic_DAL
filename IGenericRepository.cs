acb20114690a89ec5525aa048a21cf6eb9984ebe
Richard Bakos <22459180+B-Richie@users.noreply.github.com>
2017-12-26 18:57:19 -0500

    Add files via upload

---
+++ IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MusicApp.DAL.Abstract
+{
+    public interface IGenericRepository<TEntity> : IDisposable where TEntity : class
+    {
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null, string includeProperties = "");
+        IQueryable<TEntity> GetQueryable(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null, string includeProperties = "");
+        Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null, string includeProperties = "");
+        Task<TEntity> GetByIDAsync(object id);
+        TEntity GetByID(object id);
+        void Insert(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(object id);
+        void Update(TEntity entity);
+        void Save();
+
+    }
+}
